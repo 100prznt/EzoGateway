6b2cd40587f958ea043730bab8acd9bb484e1dcb
Elias Ruemmler <elias.ruemmler@de.bosch.com>
2020-02-07 11:56:09 +0100

    Clean code

--- source/EzoGateway/Config/UiConfig.cs
+++ source/EzoGateway/Config/UiConfig.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace EzoGateway.Config
 {
+    /// <summary>
+    /// Appearance settings for build-in GUI and web-interface
+    /// </summary>
     public class UiConfig
     {
+        /// <summary>
+        /// User specified device name, showed in GUI (e.g. "My own pool")
+        /// </summary>
         public string DeviceName { get; set; }
     }
 }
