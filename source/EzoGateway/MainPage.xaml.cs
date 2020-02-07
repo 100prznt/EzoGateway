d94112637f67ccadc975c079a9df91f29deec580
Elias Ruemmler <elias.ruemmler@de.bosch.com>
2020-02-07 11:09:29 +0100

    Set user defined device name for gui

--- source/EzoGateway/MainPage.xaml.cs
+++ source/EzoGateway/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using EzoGateway.Server;
+using System.Reflection;
 
 // Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
 
@@ -42,11 +43,22 @@ namespace EzoGateway
             Logger.LogWatermark();
 
             m_Controller.PropertyChanged += Controller_PropertyChanged;
+            m_Controller.ConfigIsLoadedEvent += Controller_ConfigIsLoadedEvent;
+
 
             //Apply labels
+
             tbl_Value1.Text = "pH value";
             tbl_Value2.Text = "Redox potential";
             tbl_Value3.Text = "Water temperature";
+
+            tbl_Version.Text = typeof(App).GetTypeInfo().Assembly.GetName().Version.ToString();
+        }
+
+        private void Controller_ConfigIsLoadedEvent()
+        {
+            if (m_Controller.Configuration != null && m_Controller.Configuration.Appearance != null)
+                tbl_Headline.Text = m_Controller.Configuration.Appearance.DeviceName;
         }
 
         private void Controller_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
